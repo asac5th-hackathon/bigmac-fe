918f32bf0c06b139f6ec2456826a054b259a44a0
parby8605 <lhj971011@gmail.com>
2024-07-04 04:32:41 +0900

    [Feat] 데이터 fetch 및 마커 뿌리기

--- src/app/components/MapRange/MapRange.js
+++ src/app/components/MapRange/MapRange.js
@@ -1,42 +1,38 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function MapRange() {
-  const positions = [
-    {
-      id: 1,
-      title: "카카오",
-      latlng: { lat: 33.450705, lng: 126.570677 },
-      price: 1000,
-    },
-    {
-      id: 2,
-      title: "생태연못",
-      latlng: { lat: 33.450936, lng: 126.569477 },
-      price: 2000,
-    },
-    {
-      id: 3,
-      title: "텃밭",
-      latlng: { lat: 33.450879, lng: 126.56994 },
-      price: 3000,
-    },
-    {
-      id: 4,
-      title: "근린공원",
-      latlng: { lat: 33.451393, lng: 126.570738 },
-      price: 4000,
-    },
-  ];
+  const lat = 37.544575;
+  const lng = 126.951136;
+  const [shopList, setShopList] = useState([]);
+
+  useEffect(() => {
+    const fetchStore = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/store?latitude=37.544575&longitude=126.951136&distance=400000&categoryId=1"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setShopList(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    fetchStore();
+  }, []);
 
   return (
     <Map // 지도를 표시할 Container
       center={{
         // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
+        lat: lat,
+        lng: lng,
       }}
       style={{
         // 지도의 크기
@@ -45,11 +41,11 @@ function MapRange() {
       }}
       level={3} // 지도의 확대 레벨
     >
-      {positions.map((position, index) => (
+      {shopList.map((position, index) => (
         <MapMarker
-          key={`${position.title}-${position.latlng}`}
-          position={position.latlng} // 마커를 표시할 위치
-          title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          key={`${position.id}`}
+          position={{ lat: position.latitude, lng: position.longitude }} // 마커를 표시할 위치
+          title={position.name} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
         >
           <div className="text-center">{position.price}</div>
         </MapMarker>
