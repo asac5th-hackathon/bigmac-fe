d864ee30694beed1520005681043a4d32043574a
karastke <fkdlsaos1@naver.com>
2024-07-04 01:17:03 +0900

    [Feat] 시작페이지 디자인 구현

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,16 @@
 import Example from "./components/Example";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
-
+import Title from "./components/Title";
+import LocationSearch from "./components/LocationSearch";
 export default function Home() {
   return (
-    <div className="">
-      {/** 맵 예시 코드입니다. 추후 컴포넌트 분리 추천 */}
-      <Map
-        center={{ lat: 33.5563, lng: 126.79581 }}
-        style={{ width: "100%", height: "360px" }}
-      >
-        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-          <div style={{ color: "#000" }}>Hello World!</div>
-        </MapMarker>
-      </Map>
-      <Example />
-      <Example />
-      <Example />
-      <Example />
-      <Example />
-      {/* 여기에 컴포넌트 붙이면 됩니다. */}
+    <div className="px-8">
+      <Title />
+      <div className="pt-4 text-xl font-bold space-y-0">
+         <div>합리적인 소비를 위한</div>
+         <div>준비를 시작해볼까요?</div>
+      </div>
+      <LocationSearch />
     </div>
   );
 }
